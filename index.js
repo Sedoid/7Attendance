c39e426c1fada00d3e4a5e71e34c71d7c0a953cb
Dzekewong Karlson Fonyuy <karlsedoide@gmail.com>
2020-06-17 07:25:57 -0400

    update: added chart.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs =  require('express-handlebars');
+const adminPage = require('./routes/adminProfile')
 
 const app = express()
 
@@ -9,7 +10,7 @@ app.set('view engine','handlebars');
 app.get('/',(req, res)=>{
     res.render('index');
 })
-
+app.use('/',adminPage)
 app.listen(8080,()=>{
     console.log('Server is running on Port 8080');
 })
